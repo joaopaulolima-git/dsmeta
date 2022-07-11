44c663548acef01ec5349c995263d54c86b7a072
João Paulo <jplopapa.git@gmail.com>
2022-07-11 17:33:54 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   
 
   return (
-    <div>
-      <h1>Teste</h1>
-      <NotificationButton/>
-    </div>
+    <>
+      <Header/>
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+      
+    </>
   )
 }
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css';
+import logo from '../../assets/img/logo.svg';
+
+
+export default function Header(){
+    return(
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por
+              <a href="https://github.com/joaopaulolima-git">João Paulo Lima</a>
+            </p>
+        </div>
+    </header>
+    );
+}
