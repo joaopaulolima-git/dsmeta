eadd0c7912182412a7557d05da0a3935e465b2e3
João Paulo <jplopapa.git@gmail.com>
2022-07-13 17:43:05 -0300

    API test

---
+++ backend/src/main/java/com/jpdev/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.jpdev.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jpdev.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/jpdev/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.jpdev.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpdev.dsmeta.entities.Sale;
+import com.jpdev.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSale(){
+		return repository.findAll();
+	}
+
+}
